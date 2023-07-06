05bd2d5b86ee5ae9ec78ce8314df528aaeb74dfa
Brendan Fallon <Bflawther@gmail.com>
2023-07-06 13:24:28 -0400

    adding start screen and pause UI

---
+++ GMTK2023/Assets/Scripts/PauseQuitMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseQuitMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pausemenu;
+
+
+    public void Pause(){
+        pausemenu.SetActive(true);   
+        Time.timeScale = 0f;    // pauses game
+    }
+
+    public void Resume(){
+        pausemenu.SetActive(false);   
+        Time.timeScale = 1f;    // pauses game
+    }
+    public void Quit()
+    {
+        Application.Quit();   // Quits when you're in a build
+        Debug.Log("I'm out, I quit!");
+    }
+
+    public void ChangeScene(int scene_build_index)
+    {
+        SceneManager.LoadScene(scene_build_index);
+    }
+
+
+
+
+    private void Update() {   // Escape key to pause
+        if (Input.GetKeyDown(KeyCode.Escape) )
+        {
+            if ( pausemenu.activeSelf == false )  // if pause menu if pause
+            {
+                Pause();
+            }
+            else
+            {
+                Resume();
+            }
+            
+        }
+    }
+
+}
